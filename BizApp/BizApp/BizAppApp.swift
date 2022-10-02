d0a58467df8528e173e5727869126d61da5a17dc
jp <jplatreille7@gmail.com>
2022-10-02 14:59:33 -0700

    hello world application

---
+++ BizApp/BizApp/BizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BizAppApp.swift
+//  BizApp
+//
+//  Created by Jon Latreille on 10/2/22.
+//
+
+import SwiftUI
+
+@main
+struct BizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
